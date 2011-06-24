204b1e2c048fbfde45a2cfb3872514947e4a9068
Patrick Hurley <phurley@gmail.com>
2011-06-24 13:46:42 -0400

    Allow floating rails version 3.0.x

--- lib/action_web_service.rb
+++ lib/action_web_service.rb
@@ -31,10 +31,10 @@ begin
   require 'action_dispatch/routing'
 rescue LoadError
   require 'rubygems'
-  gem 'activesupport', '3.0.1'
-  gem 'actionpack', '3.0.1'
-  gem 'activerecord', '3.0.1'
-  gem 'activesupport', '3.0.1'
+  gem 'activesupport', '~> 3.0'
+  gem 'actionpack', '~> 3.0'
+  gem 'activerecord', '~> 3.0'
+  gem 'activesupport', '~> 3.0'
 end
 
 $:.unshift(File.dirname(__FILE__) + "/action_web_service/vendor/")
